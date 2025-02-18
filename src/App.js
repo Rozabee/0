94f68a6091a7f1c1ab90bf8e1bdd2be15203c22f
Bean <baldonado.beanicole1013@gmail.com>
2025-02-18 11:38:29 +0800

    clean structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [braincellCount, setBraincellCount] = useState(0);
+  const cellCount = () => {
+    setBraincellCount(braincellCount + 1);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>brain cell count : {braincellCount}</h1>
+        <button onClick={(cellCount)}>increase braincell</button>
     </div>
   );
 }
