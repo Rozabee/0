2648bbd1836d3265990f6b16c2507097f8a3980b
Bean <baldonado.beanicole1013@gmail.com>
2025-02-18 13:36:38 +0800

    move to props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import Brain from './components/brain';
 import './App.css';
 
-function App() {
-
+function App() { 
+  const showBrain = true;
+  
   const [braincellCount, setBraincellCount] = useState(0);
   const cellCount = () => {
     setBraincellCount(braincellCount + 1);
   }
   
   return (
+    // parent div
     <div className="App">
+
+      { showBrain ? <Brain/> : <p>no brain</p> }
+      
        <h1>brain cell count : {braincellCount}</h1>
         <button onClick={(cellCount)}>increase braincell</button>
     </div>
---
+++ src/components/brain.js
@@ -0,0 +1,6 @@
+function brain() {
+    return <h3>My brain</h3>;
+
+}
+// allows to Export the Employee component into other files 
+export default brain;
