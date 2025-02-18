66af23686e1affa2d73e12fd5fb4b5f3d3a6787e
Bean <baldonado.beanicole1013@gmail.com>
2025-02-18 21:36:15 +0800

    Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import Cats from './components/Cat';
-import './App.css';
+import './index.css'; 
 
 function App() { 
 
@@ -14,32 +14,46 @@ const cellCount = () => {
 
 return (
   // parent div
-<div className="App">
-<h1> My Cats</h1>
-<p>Name</p>
+<div className="App h-24 max-w-[1240] mx-auto p-4">
+<h2 className='w-full text-3xl font-bold text-pink-600 mb-1'> My Cats</h2>
   <div className='cats'>
     { showCats ? (
       <>
-      <input type='text' onChange={(e) => {console.log(e.target.value); setAttitude(e.target.value)}}/>
-    < Cats
+      <input  className='mt-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 px-2' type='text' placeholder='ouieeaaiouiai' onChange={(e) => {console.log(e.target.value); setAttitude(e.target.value)}}/>
+      <p className='text-pink-300 mt-4'>LIST OF MY CATS</p>
+      <div className="flex flex-wrap justify-center bg-#1d1c1b p-4"> 
+    < Cats 
       name = "Lucky" 
       color = "Black" 
+      img = "\imgs\lucky.jpg"
       age = {5}
       role = {attitude}
       />
     < Cats
       name = "Curry" 
-      color = "Yellow and Black" 
+      color = "Yellow" 
+      img= "/imgs/curry.jpg"
       age = {4}
       role = {attitude}
       />
     < Cats
       name = "Cheese" 
       color = "Yellow" 
+      img = "/imgs/cheese.jpg"
       age = {4.5}
       role = {attitude}
       />
+    < Cats
+      name = "Cheese" 
+      color = "Yellow" 
+      img = "/imgs/cheese.jpg"
+      age = {4.5}
+      role = {attitude}
+      />
+   
+      </div>
       </>
+       
     )
       : 
       (<h3> No Cats</h3>)
@@ -50,9 +64,11 @@ return (
 
 
 <hr/>
-<h4>Brain cell count: {(braincellCount)} </h4>
-<button onClick={(cellCount)}>increase braincell</button>
+<div className= 'flex justify-center items-center gap-5 p-4'>
+<h4 className='text-white'>Brain cell count : {(braincellCount)} </h4>
+<button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-300 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={(cellCount)}>increase braincell</button>
 
+  </div>
 </div>
   );
 }
--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,13 +1,27 @@
 function Cat(props) {
 //rule 
-    return (
-    <>
-        <h3>{props.name}</h3>
-        <p>Color : {props.color}</p>
-        <p>Age : {props.age}</p>
-        <p>Role : {props.role ? props.role : "No Role"}</p>
-    </>
-    )
+return (
+    <div class="m-2 py-8 px-8 max-w-sm space-y-2 bg-#1d1c1b rounded-xl shadow-lg sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:gap-x-6">
+
+    <img class="object-cover rounded-full h-[100px] w-[100px] shadow-lg block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="cat" />
+
+    <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+            <p class="text-lg text-white font-semibold">
+                {props.name}
+            </p>
+            <p class="text-slate-500 font-medium">
+                Age : {props.age}
+            </p>
+       
+        </div>
+        <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-300 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        Edit
+        </button>
+    </div>
+    <hr/>
+    </div>
+)
 }
 // allows to Export the Employee component into other files 
 export default Cat;
